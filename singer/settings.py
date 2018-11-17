2946ecf34af88f759722c7a1f6bd91ed7ef1cddf
YUND4 <30837558+YUND4@users.noreply.github.com>
2018-11-16 21:21:02 -0500

    Update settings.py
    Postgresql

--- singer/settings.py
+++ singer/settings.py
@@ -98,9 +98,13 @@ WSGI_APPLICATION = 'singer.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
